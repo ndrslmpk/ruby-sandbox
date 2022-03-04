87f3a1af0c51d1ed398e76ccb480b87ae52758ed
Andreas Lempik <andreaslempik@MBP-von-Andreas.lan>
2022-03-04 11:45:46 +0100

    adding resources and link_to for routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
+  # resources "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
