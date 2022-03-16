0d57ca9d176b5c0b4bb0894614b0506e46ee43eb
ndrslmpk <andreas.lempik@gmail.com>
2022-03-16 10:27:53 +0100

    added basic controller logic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,25 @@ class ArticlesController < ApplicationController
   def show 
     @article = Article.find(params[:id])
   end
+
+  def new 
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title:"...", body:"...")
+    # @articleDefault = Article.new(title:"Default Title", body:"Default Body")
+
+    if @article.save
+      redirect_to @article
+    else
+      # render :new(.html.erb) and return the status(code): of an unprocessable_entity Status code: 422
+      render :new, status: :unprocessable_entity 
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body) # filters params
+    end
 end
