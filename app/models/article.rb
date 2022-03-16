57f23632222af60ede22042fad71d53e2c1799f8
ndrslmpk <andreas.lempik@gmail.com>
2022-03-16 10:28:44 +0100

    added basic article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+  # Validations inside this data-based model are used to handle invalid user inputs  
+  # params:
+  #   @validates <TABLE COLUMN / ATTRIBUTE>: IDENTIFIER (STRING) 
+  #   @presence: NOT NULL (BOOLEAN) 
+  #   @length; CONSTRAINT (INT)
+  validates :title, presence: true 
+  validates :body, presence: true, length: { minimum: 10 } # WHY does it have to be enclosed in curly brackets?
 end
