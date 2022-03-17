fd8677e1df6e37b2119fbdcf549de81095f53d6b
ndrslmpk <andreas.lempik@gmail.com>
2022-03-17 12:39:15 +0100

    modified article model and added constraints

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,11 @@
 class Article < ApplicationRecord
+  has_many :comments
+
   # Validations inside this data-based model are used to handle invalid user inputs  
   # params:
   #   @validates <TABLE COLUMN / ATTRIBUTE>: IDENTIFIER (STRING) 
-  #   @presence: NOT NULL (BOOLEAN) 
-  #   @length; CONSTRAINT (INT)
+  #     - presence: NOT NULL (BOOLEAN) 
+  #     - length; CONSTRAINT (INT)
   validates :title, presence: true 
   validates :body, presence: true, length: { minimum: 10 } # WHY does it have to be enclosed in curly brackets?
 end
