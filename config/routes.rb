6bc6f3c8d07ea05204420554d1d1ede6220546c9
ndrslmpk <andreas.lempik@gmail.com>
2022-03-17 12:45:01 +0100

    added nested resource comment to make comments depend on articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,24 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  resources :articles
-  # resources "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
+  resources :articles do
+
+    # NESTED RESOURCES 
+    # articles inside comments and creates a hierarchical structure  
+    # This will automatically generate a routing dependency that will require
+    # an article within a route like this:
+    # ../articles/:articles_id/comments/....
+    # ../articles/:articles_id/comments/
+    # ../articles/:articles_id/comments/new
+    # ../articles/:articles_id/comments/id
+    # ../articles/:articles_id/comments/:id/edit
+
+    resources :comments
+  end
+
+
+  get "/articles/topfive", to: "articles#topfive"
+  get "/articles/search/:id", to: "articles#show" # :id is a route parameter
+  get "/articles/first", to: "articles#first"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
